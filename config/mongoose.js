a1e5a2eaca8bb3c15df82d44359f9eaf3579a983
jelitte <jelitte@naver.com>
2018-01-05 15:49:53 +0900

    base server bug fix

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,1 @@
-var config = require('./config'),
-    mongoose = require('mongoose');
-
-console.log(config.db);
-
-module.exports = function(){
-    var db = mongoose.connect(config.db,{
-        useMongoClient: true,
-    });
-    require('../app/models/user.server.model');
-   
-    return db;
-};
+var config = require('./config'),    mongoose = require('mongoose');console.log(config.db);module.exports = function(){    var db = mongoose.connect(config.db,{        useMongoClient: true,    });    require('../app/models/user.server.model');       return db;};
